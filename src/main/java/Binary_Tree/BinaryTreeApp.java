8e8bb504c825cf5b6f26e716a6b16f98deb18227
Muhammad Reziq Darusman <72184597+reziqvins@users.noreply.github.com>
2021-06-29 09:34:28 +0700

    Add files via upload

--- src/main/java/Binary_Tree/BinaryTreeApp.java
+++ src/main/java/Binary_Tree/BinaryTreeApp.java
@@ -5,34 +5,30 @@ public class BinaryTreeApp {
         
         TreeNode node;
         
-        node = new TreeNode(20);
+        node = new TreeNode(50);
         tree.insert(node);
         
         node = new TreeNode (30);
         tree.insert(node);
         
-        node = new TreeNode (40);
+        node = new TreeNode (20);
         tree.insert(node);
         
-        node = new TreeNode (50);
+        node = new TreeNode (40);
         tree.insert(node);
         
         node = new TreeNode (70);
         tree.insert(node);
-
-        
-        System.out.print("Traversal dengan PreOrder : ");
-        tree.preOrder();
-        System.out.println("");
-        System.out.print("\nTraversal dengan InOrder : ");
-        tree.postOrder();
-        System.out.println("");
-        System.out.print("\nTraversal dengan PostOrder : ");
+        System.out.print("Traversal dengan InOrder : ");
         tree.inOrder();
-        System.out.println("");
+        System.out.print("\nTraversal dengan PreOrder: ");
+        tree.preOrder();
+        System.out.print("\nTraversal dengan PostOrder: ");
+        tree.postOrder();
         System.out.println();
+        
+        
+        
         
     }
 }
-
-
